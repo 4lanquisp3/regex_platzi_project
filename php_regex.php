527a7c02985b6b460931d66feefcb174ab3d04ad
Alan <4lanquisp3@gmail.com>
2020-08-31 12:10:51 -0400

    Regex con PHP completo

--- php_regex.php
+++ php_regex.php
@@ -5,14 +5,24 @@ $file = fopen("./results.csv", "r");
 $match = 0;
 $nomatch = 0;
 
+$t= time();
+
 while(!feof($file)){
    $line = fgets($file);
+   // 1873-03-08,England,Scotland,4,2,Friendly,London,England,FALSE
    if(preg_match(
-      '/^2018\-01\-(\d\d),.*$/',
+      '/^(\d{4}\-\d\d\-\d\d),(.+),(.+),(\d+),(\d+),.*$/i',
       $line,
       $m
    )){
-      print_r($m);
+      if($m[4] == $m[5]){
+        echo "empate: ";        
+      }elseif($m[4] > $m[5]){
+        echo "local:   "; // espacios extra para que el tab sea correcto
+      }else{
+        echo "visitante: ";
+      }
+      printf("\t%s, %s [%d-%d]\n",$m[2],$m[3],$m[4],$m[5]);
       $match++;
    }else{
      $nomatch++;
@@ -21,5 +31,7 @@ while(!feof($file)){
 
 fclose($file);
 printf("\n\nmatch: %d\nno match: %d\n", $match, $nomatch);
+
+printf("tiempo: %d Seg.\n", time() - $t);
 
 ?>
